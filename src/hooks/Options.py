804210a14eb86ceaa20a55a89f65955ce3783e69
PaperDrake148 <135507203+PaperDrake148@users.noreply.github.com>
2026-08-14 13:59:28 +0200

    added locations, removed world map region

--- src/hooks/Options.py
+++ src/hooks/Options.py
@@ -3,6 +3,7 @@ from src.Options import Option, FreeText, NumericOption, Toggle, DefaultOnToggle
 # These helper methods allow you to determine if an option has been set, or what its value is, for any player in the multiworld
 from ..Helpers import is_option_enabled, get_option_value
 from typing import Type, Any
+from Options import OptionSet
 
 
 ####################################################################
@@ -25,12 +26,17 @@ from typing import Type, Any
 # To add an option, use the before_options_defined hook below and something like this:
 #   options["total_characters_to_win_with"] = TotalCharactersToWinWith
 #
-class TotalCharactersToWinWith(Range):
-    """Instead of having to beat the game with all characters, you can limit locations to a subset of character victory locations."""
-    display_name = "Number of characters to beat the game with before victory"
-    range_start = 10
-    range_end = 50
-    default = 50
+# class TotalCharactersToWinWith(Range):
+#     """Instead of having to beat the game with all characters, you can limit locations to a subset of character victory locations."""
+#     display_name = "Number of characters to beat the game with before victory"
+#     range_start = 10
+#     range_end = 50
+#     default = 50
+
+class EnabledStages(OptionSet):
+    """Stages that will be eligible for checks"""
+    display_name = "Enabled Stages"
+    valid_keys =
 
 # This is called before any manual options are defined, in case you want to define your own with a clean slate or let Manual define over them
 def before_options_defined(options: dict[str, Type[Option[Any]]]) -> dict[str, Type[Option[Any]]]:
